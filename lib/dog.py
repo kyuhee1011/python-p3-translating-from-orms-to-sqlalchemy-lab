2ef680da703051be4173b47457d34f521bea6d77
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-08-06 22:37:39 -0400

    warning sign

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,48 @@
-from models import Dog
+from models import Dog , Base
+from sqlalchemy import (create_engine,Column,Integer, String) 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from testing.conftest import SQLITE_URL
 
-def create_table(base):
-    pass
+engine = create_engine(SQLITE_URL)
+Base.metadata.create_all(engine)
+Session = sessionmaker(engine)
+session = Session()
+Base = declarative_base()
+
+class Dog (Base):
+    __tablename__="dogs"
+    id = Column(Integer(),  primary_key=True)
+    name = Column(String())
+    breed = Column(String()) 
+
+def create_table(Base, engine):
+    if __name__== '__main__':
+
+     Base.metadata.create_all(engine)
+    
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session.query(Dog).first().name
 
 def get_all(session):
-    pass
+    all_dogs = []
+    all_dogs = session.query(Dog).all()
+    return all_dogs
 
 def find_by_name(session, name):
-    pass
+    return session.query (Dog).filter (Dog.name==name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query (Dog).filter (Dog.id==id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query (Dog).filter (Dog.name==name and Dog.breed ==breed).first()
+    
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed=breed
+    session.query(Dog).update({Dog.breed : breed})
+    session.commit()
